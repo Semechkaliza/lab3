998716bac2137df004394f60c32a7c201420b5ad
Lizaveta Grishkina <grishkinaelizaveta@gmail.com>
2018-03-04 19:31:24 +0300

    lab 304

---
+++ lab3/customers_insert.sql
@@ -0,0 +1,7 @@
+insert into customers (name,adress,phone,person) value ('Vasya Fruit','Minsk','80291234567','Vasya'),
+('Bella&Co','Vitebsk','+375231234567','Inna'),
+('Abracadabra','Minsk','+375299876541','Alla'),
+('Mila','Minsk','562314785','Pavel'),
+('Summer','Grodno','80291234567','Svetlana'),
+('Paradise','Homel','+375441235698','Irina'),
+('Polly','Minsk','802912341234','Anna');
